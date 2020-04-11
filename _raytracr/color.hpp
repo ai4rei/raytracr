a4d90e2575490621228d5adb309c163a2ddb5ca7
Ai4rei <ai4rei@users.noreply.github.com>
2020-04-11 09:43:33 +0200

    Updated CColor.
    * Moved value limiting to value retrieval, to allow for future stupidity, like anti-aliasing.

--- _raytracr/color.hpp
+++ _raytracr/color.hpp
@@ -15,17 +15,17 @@ public:
 
     CColor operator+(const CColor& clrOther) const
     {
-        return CColor(min(1.0f, m_nR+clrOther.m_nR), min(1.0f, m_nG+clrOther.m_nG), min(1.0f, m_nB+clrOther.m_nB));
+        return CColor(m_nR+clrOther.m_nR, m_nG+clrOther.m_nG, m_nB+clrOther.m_nB);
     }
 
     CColor operator-(const CColor& clrOther) const
     {
-        return CColor(max(0.0f, m_nR-clrOther.m_nR), max(0.0f, m_nG-clrOther.m_nG), max(0.0f, m_nB-clrOther.m_nB));
+        return CColor(m_nR-clrOther.m_nR, m_nG-clrOther.m_nG, m_nB-clrOther.m_nB);
     }
 
-    CColor operator*(const float nIntensity) const
+    CColor operator*(const float nScale) const
     {
-        return CColor(m_nR*nIntensity, m_nG*nIntensity, m_nB*nIntensity);
+        return CColor(m_nR*nScale, m_nG*nScale, m_nB*nScale);
     }
 
     CColor operator*(const CColor& clrOther) const
@@ -33,12 +33,17 @@ public:
         return CColor(m_nR*clrOther.m_nR, m_nG*clrOther.m_nG, m_nB*clrOther.m_nB);
     }
 
+    CColor operator/(const float nScale) const
+    {
+        return CColor(m_nR/nScale, m_nG/nScale, m_nB/nScale);
+    }
+
     CColor operator~() const
     {
         return CColor(1.0f-m_nR, 1.0f-m_nG, 1.0f-m_nB);
     }
 
-    float R() const { return m_nR; }
-    float G() const { return m_nG; }
-    float B() const { return m_nB; }
+    float R() const { return max(0.0f, min(1.0f, m_nR)); }
+    float G() const { return max(0.0f, min(1.0f, m_nG)); }
+    float B() const { return max(0.0f, min(1.0f, m_nB)); }
 };
