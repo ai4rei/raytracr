00e4120082b4b5a52c35919c4f37c83fbd16d123
Ai4rei <ai4rei@users.noreply.github.com>
2020-05-11 12:24:53 +0200

    Updated DirectX9 reference test.
    + Added light, plane, spheres and z-testing.
    * No longer keeps IDirectD3D9 around.
    ! Fixed back buffer size would not be updated when the window is set up.

--- testcase2dx.cpp
+++ testcase2dx.cpp
@@ -1,17 +1,9 @@
-#include <d3d9.h>
 #include <windows.h>
 #include <windowsx.h>
+#include <d3d9.h>
+#include <d3dx9.h>
 
 #include "simplewnd.tpp"
-
-HRESULT WINAPI Direct3DCreate9(UINT SDKVersion, IDirect3D9** lppD3DOut)
-{
-    IDirect3D9* lpD3D = Direct3DCreate9(SDKVersion);
-
-    lppD3DOut[0] = lpD3D;
-
-    return lpD3D!=NULL ? S_OK : D3DERR_NOTAVAILABLE;
-}
 
 class CCoInitialize
 {
@@ -41,64 +33,135 @@ private:
     typedef CSimpleWindow CSuper;
 
 protected:
-    IDirect3D9* m_lpD3D;
     IDirect3DDevice9* m_lpDD;
+
+    ID3DXMesh* m_lpSphere1Mesh;
+    ID3DXMesh* m_lpSphere01Mesh;
 
 public:
     virtual ~CTestWindow()
     {
-        UninitializeDirectX(true);
     }
 
     CTestWindow(HINSTANCE hInstance)
         : CSimpleWindow(hInstance, 640, 480, WS_VISIBLE|WS_POPUPWINDOW|WS_CAPTION|WS_MINIMIZEBOX)
-        , m_lpD3D(nullptr)
         , m_lpDD(nullptr)
+        , m_lpSphere1Mesh(nullptr)
+        , m_lpSphere01Mesh(nullptr)
     {
     }
 
     void UninitializeDirectX(const bool bFinal = false)
     {
+        if(m_lpSphere01Mesh!=nullptr)
+        {
+            m_lpSphere01Mesh->Release();
+            m_lpSphere01Mesh = nullptr;
+        }
+
+        if(m_lpSphere1Mesh!=nullptr)
+        {
+            m_lpSphere1Mesh->Release();
+            m_lpSphere1Mesh = nullptr;
+        }
+
         if(m_lpDD!=nullptr)
         {
             m_lpDD->Release();
             m_lpDD = nullptr;
         }
+    }
 
-        if(bFinal && m_lpD3D!=nullptr)
+    bool InitializeDevice()
+    {
+        if(FAILED(m_lpDD->SetRenderState(D3DRS_ZENABLE, D3DZB_TRUE)))
         {
-            m_lpD3D->Release();
-            m_lpD3D = nullptr;
+            return false;
         }
+
+        // R.AddLight(Raytracer::CreateLight(Raytracer::CreateVector3d(+9.0f, +9.0f, +0.0f), Raytracer::CreateColor(1.0f, 1.0f, 1.0f), 50.0f));
+        D3DLIGHT9 Li = { D3DLIGHT_POINT };
+        Li.Diffuse =
+        Li.Specular = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+        Li.Ambient = D3DXCOLOR(0.0f, 0.0f, 0.0f, 1.0f);
+        Li.Position = D3DXVECTOR3(9.0f, 9.0f, 0.0f);
+        Li.Range = 50.0f;
+        Li.Falloff = 1.0f;
+        Li.Attenuation1 = 0.1f;
+
+        if(FAILED(m_lpDD->SetLight(0, &Li)))
+        {
+            return false;
+        }
+
+        if(FAILED(m_lpDD->LightEnable(0, TRUE)))
+        {
+            return false;
+        }
+
+        if(FAILED(m_lpDD->SetRenderState(D3DRS_LIGHTING, TRUE)))
+        {
+            return false;
+        }
+
+        if(FAILED(m_lpDD->SetRenderState(D3DRS_AMBIENT, D3DCOLOR_COLORVALUE(0.2f, 0.2f, 0.2f, 1.0f))))
+        {
+            return false;
+        }
+
+        D3DXMATRIX mtxV;
+        D3DXVECTOR3 ovecEye(+0.0f, +1.0f, -10.0f);
+        D3DXVECTOR3 ovecTarget(+0.0f, +1.0f, -9.0f);
+        D3DXVECTOR3 uvecUp(+0.0f, +1.0f, +0.0f);
+
+        D3DXMatrixLookAtLH(&mtxV, &ovecEye, &ovecTarget, &uvecUp);
+
+        if(FAILED(m_lpDD->SetTransform(D3DTS_VIEW, &mtxV)))
+        {
+            return false;
+        }
+
+        if(FAILED(D3DXCreateSphere(m_lpDD, 1.0f, 20, 20, &m_lpSphere1Mesh, NULL)))
+        {
+            return false;
+        }
+
+        if(FAILED(D3DXCreateSphere(m_lpDD, 0.1f, 20, 20, &m_lpSphere01Mesh, NULL)))
+        {
+            return false;
+        }
+
+        return true;
     }
 
     bool InitializeDirectX(HWND hWnd)
     {
-        HRESULT hr;
-
-        if(m_lpD3D==nullptr)
-        {
-            hr = Direct3DCreate9(D3D_SDK_VERSION, &m_lpD3D);
-
-            if(FAILED(hr))
-            {
-                return false;
-            }
-        }
-
         if(m_lpDD==nullptr)
         {
-            D3DPRESENT_PARAMETERS D3dpp = { 0 };
+            IDirect3D9* lpD3D = Direct3DCreate9(D3D_SDK_VERSION);
 
-            D3dpp.Windowed = TRUE;
-            D3dpp.SwapEffect = D3DSWAPEFFECT_FLIP;
+            if(lpD3D==nullptr)
+            {
+                return false;
+            }
 
-            hr = m_lpD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &D3dpp, &m_lpDD);
+            D3DPRESENT_PARAMETERS PP = { 0 };
+
+            PP.Windowed = TRUE;
+            PP.SwapEffect = D3DSWAPEFFECT_FLIP;
+            PP.EnableAutoDepthStencil = TRUE;
+            PP.AutoDepthStencilFormat = D3DFMT_D16;
+
+            HRESULT hr = lpD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &PP, &m_lpDD);
+
+            lpD3D->Release();  // no longer needed
 
             if(FAILED(hr))
             {
                 return false;
             }
+
+            return InitializeDevice();
         }
 
         return true;
@@ -111,25 +174,74 @@ public:
         return InitializeDirectX(hWnd);
     }
 
+    void RenderSphere(ID3DXMesh* lpSphere, const D3DVECTOR& ovecPosition, const D3DCOLORVALUE& clrMaterial)
+    {
+        D3DXMATRIX mtxWorld;
+
+        D3DXMatrixTranslation(&mtxWorld, ovecPosition.x, ovecPosition.y, ovecPosition.z);
+        m_lpDD->SetTransform(D3DTS_WORLD, &mtxWorld);
+
+        D3DMATERIAL9 matMaterial = { 0 };
+
+        matMaterial.Diffuse = matMaterial.Ambient = clrMaterial;
+        m_lpDD->SetMaterial(&matMaterial);
+
+        lpSphere->DrawSubset(0);
+    }
+
     void Render()
     {
-        if(m_lpDD==nullptr)
+        m_lpDD->Clear(0, NULL, D3DCLEAR_TARGET|D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
+
+        if(!FAILED(m_lpDD->BeginScene()))
         {
-            return;
+            D3DXMATRIX matWorld;
+
+            //R.AddObject(Raytracer::CreatePlane(Raytracer::CreateVector3d(+0.0f, +0.0f, +0.0f), Raytracer::CreateVector3d(+0.0f, +1.0f, +0.0f), Raytracer::CreateColor(1.0f, 1.0f, 1.0f)));
+            const struct XYZ_N_VECTOR
+            {
+                float x, y, z;
+                float nx, ny, nz;
+            }
+            avtxFan[] =
+            {
+                {   +0.0f, +0.0f,   +0.0f, 0.0f, 1.0f, 0.0f },
+                { -100.0f, +0.0f, -100.0f, 0.0f, 1.0f, 0.0f },
+                { -100.0f, +0.0f, +100.0f, 0.0f, 1.0f, 0.0f },
+                { +100.0f, +0.0f, +100.0f, 0.0f, 1.0f, 0.0f },
+                { +100.0f, +0.0f, -100.0f, 0.0f, 1.0f, 0.0f },
+                { -100.0f, +0.0f, -100.0f, 0.0f, 1.0f, 0.0f },
+            };
+            D3DMATERIAL9 matWhite = { 0 };
+            matWhite.Diffuse = matWhite.Ambient = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+            m_lpDD->SetMaterial(&matWhite);
+            m_lpDD->SetFVF(D3DFVF_XYZ|D3DFVF_NORMAL);
+            m_lpDD->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, _countof(avtxFan)-2, &avtxFan, sizeof(avtxFan[0]));
+
+            //R.AddObject(Raytracer::CreateSphere(Raytracer::CreateVector3d(+0.0f, +0.0f, +0.0f), 0.1f, Raytracer::CreateColor(1.0f, 0.0f, 0.0f)));
+            RenderSphere(m_lpSphere01Mesh, D3DXVECTOR3(+0.0f, +0.0f, 0.0f), D3DXCOLOR(1.0f, 0.0f, 0.0f, 1.0f));
+
+            //R.AddObject(Raytracer::CreateSphere(Raytracer::CreateVector3d(+2.0f, +1.0f, -3.0f), 1.0f, Raytracer::CreateColor(0.25f, 0.25f, 0.75f)));
+            RenderSphere(m_lpSphere1Mesh, D3DXVECTOR3(+2.0f, +1.0f, +3.0f), D3DXCOLOR(0.25f, 0.25f, 0.75f, 1.0f));
+
+            //R.AddObject(Raytracer::CreateSphere(Raytracer::CreateVector3d(-2.0f, +1.0f, -3.0f), 1.0f, Raytracer::CreateColor(0.75f, 0.25f, 0.25f)));
+            RenderSphere(m_lpSphere1Mesh, D3DXVECTOR3(-2.0f, +1.0f, +3.0f), D3DXCOLOR(0.75f, 0.25f, 0.25f, 1.0f));
+
+            //R.AddObject(Raytracer::CreateSphere(Raytracer::CreateVector3d(+0.0f, +1.0f, -2.0f), 1.0f, Raytracer::CreateColor(0.25f, 0.75f, 0.25f)));
+            RenderSphere(m_lpSphere1Mesh, D3DXVECTOR3(+0.0f, +1.0f, +2.0f), D3DXCOLOR(0.25f, 0.75f, 0.25f, 1.0f));
+
+            m_lpDD->EndScene();
         }
 
-        m_lpDD->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 127, 127), 1.0f, 0);
-        m_lpDD->BeginScene();
-        m_lpDD->EndScene();
         m_lpDD->Present(NULL, NULL, NULL, NULL);
     }
 
     virtual BOOL WndProcOnCreate(HWND hWnd, LPCREATESTRUCT lpCreateStruct)
     {
-        if(CSuper::WndProcOnCreate(hWnd, lpCreateStruct))
+        if(InitializeDirectX(hWnd))
         {
-            if(InitializeDirectX(hWnd))
-            {
+            if(CSuper::WndProcOnCreate(hWnd, lpCreateStruct))
+            {// this has to come last, because it triggers onWindowPosChanged
                 SetWindowText(hWnd, "DirectX TestCase (press ESC to exit)");
                 return TRUE;
             }
@@ -142,6 +254,27 @@ public:
     {
         Render();
         ValidateRect(hWnd, NULL);
+    }
+
+    virtual VOID WndProcOnWindowPosChanged(HWND hWnd, const WINDOWPOS* lpWP)
+    {
+        RECT rcWnd;
+
+        GetClientRect(hWnd, &rcWnd);
+
+        D3DPRESENT_PARAMETERS PP = { 0 };
+        PP.Windowed = TRUE;
+        PP.SwapEffect = D3DSWAPEFFECT_FLIP;
+        PP.EnableAutoDepthStencil = TRUE;
+        PP.AutoDepthStencilFormat = D3DFMT_D16;
+        m_lpDD->Reset(&PP);
+        InitializeDevice();
+
+        D3DXMATRIX mtxP;
+        D3DXMatrixPerspectiveFovLH(&mtxP, D3DX_PI/4, float(rcWnd.right-rcWnd.left)/float(rcWnd.bottom-rcWnd.top), 1.0f, 100.0f);
+        m_lpDD->SetTransform(D3DTS_PROJECTION, &mtxP);
+
+        CSuper::WndProcOnWindowPosChanged(hWnd, lpWP);
     }
 };
 
