c02bdb9cf907611d22791ba6bb550692fe0011e7
Ai4rei <ai4rei@users.noreply.github.com>
2020-05-12 00:56:44 +0200

    Updated Travis CI config for DX8.

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/ai4rei/D3D9SDK-Mini.git
+git clone https://github.com/ai4rei/D3D8SDK-Mini.git
--- .travis.cmd
+++ .travis.cmd
@@ -1,4 +1,4 @@
 @ECHO OFF
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars32.bat"
-SET DXSDK=D3D9SDK-Mini
+SET DXSDK=D3D8SDK-Mini
 NMAKE /P CLEAN ALL
